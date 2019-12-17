f1099f1cd7a3265accc64b54c758ad4a7185dc81
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-17 00:28:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,40 +40,40 @@ monopoly = {
 
 	     :rent_in_dollars => {
 
-	    :one_piece_owned=>25,
+	    :one_piece_owned => 25,
 
-      :two_pieces_owned=>50,
+      :two_pieces_owned => 50,
 
-      :three_pieces_owned=>100,
+      :three_pieces_owned => 100,
 
-      :four_pieces_owned=>200
+      :four_pieces_owned => 200
 
 	     },
 
 	     :names => {
 
-	       :reading_railroad=>
+	       :reading_railroad =>
 
       {
 
            
       },
 
-           :pennsylvania_railroad=>
+           :pennsylvania_railroad =>
 
       {
 
           
       },
 
-           :b_and_o_railroad=>
+           :b_and_o_railroad =>
 
       {
 
            
       },
 
-            :shortline_railroad=>
+            :shortline_railroad =>
 
       {
 
@@ -102,44 +102,44 @@ monopoly = {
 
 	    :one_piece_owned=>25,
 
-      :two_pieces_owned=>50,
+      :two_pieces_owned => 50,
 
-      :three_pieces_owned=>100,
+      :three_pieces_owned => 100,
 
-      :four_pieces_owned=>200
+      :four_pieces_owned => 200
 
 	     },
 
 	     :names => {
 
-	        :reading_railroad=>
+	        :reading_railroad =>
 
       {
  
-          "mortgage_value"=>"$100"
+          "mortgage_value" => "$100"
 
       },
-           :pennsylvania_railroad=>
+           :pennsylvania_railroad =>
 
       {
 
-           "mortgage_value"=>"$200"
+           "mortgage_value" => "$200"
 
       },
 
-           :b_and_o_railroad=>
+           :b_and_o_railroad =>
 
       {
 
-            "mortgage_value"=>"$400"
+            "mortgage_value" => "$400"
 
       },
 
-            :shortline_railroad=>
+            :shortline_railroad =>
 
       {
 
-            "mortgage_value"=>"$800"
+            "mortgage_value" => "$800"
 
       }
 
